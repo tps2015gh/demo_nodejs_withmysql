8235aa4992101f6c829382682cd342f7677fa3b3
Thitipong Samranvanich <tpsxx2015@gmail.com>
2019-06-27 05:04:46 +0700

    move default controller to script_app.ts

--- dist/script_all.js
+++ dist/script_all.js
@@ -1,5 +1,8 @@
 /* script_app.js start */
 var module = angular.module("myApp", []);
+module.controller("myAppController", function () {
+    this.listOfItem = [1, 2, 3];
+});
 /*   script_comp1.ts start */
 module.component("item", {
     template: ""
--- script_app.ts
+++ script_app.ts
@@ -1,3 +1,7 @@
 /* script_app.js start */
 
 var module = angular.module("myApp", []);
+
+module.controller("myAppController", function () {
+    this.listOfItem = [1, 2, 3];
+});
